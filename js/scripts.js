de2ee3a4317823a17ed5de6d2f1461d2298007f2
Krzysztof Barański <Krzysztof Baraski>
2017-03-26 16:12:30 +0200

    dodatni ujemy

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// plik scripts.js
+
+var a = -20;
+var b = 3;
+var value = ((a * a) + (2 * a * b) - (b * b));
+console.log('Wynik dzialania: ' + value);
+console.log(value > 0 ? 'Wynik dodatni' : 'Wynik ujemny');
